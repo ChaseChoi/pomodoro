c49a6f632eceb7651560ff0665a83814eaac9b7e
ChaseChoi <caiguihao@gmail.com>
2018-06-21 15:59:26 +0800

    add CoreDataManager class

---
+++ pomodoro/Manager/CoreDataManager.swift
@@ -0,0 +1,54 @@
+//
+//  CoreDataManager.swift
+//  pomodoro
+//
+//  Created by Chase Choi on 2018/6/21.
+//  Copyright © 2018 Chase Choi. All rights reserved.
+//
+
+import CoreData
+
+class CoreDataManager {
+    // MARK: - Properties
+    private let modelName: String
+    
+    private(set) lazy var managedObjectContext: NSManagedObjectContext = {
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
+        return managedObjectContext
+    }()
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        guard let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd") else {
+            fatalError("Unable to find data model")
+        }
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Unable to load data model")
+        }
+        return managedObjectModel
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        
+        // Helpers
+        let fileManager = FileManager.default
+        let storeName = "\(self.modelName).sqlite"
+        
+        let documentsDirectoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        
+        // URL persistent store
+        let persistentStoreURL = documentsDirectoryURL.appendingPathComponent(storeName)
+        
+        do {
+            try persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: persistentStoreURL, options: nil)
+        } catch {
+            fatalError("Unable to add persitent store")
+        }
+        return persistentStoreCoordinator
+    }()
+    
+    // MARK: - Initialization
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+}
