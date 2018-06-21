9f6cf358f1c0ef8b51886d1b476e0df67cec069e
ChaseChoi <caiguihao@gmail.com>
2018-06-21 14:30:06 +0800

    Initial Commit

---
+++ pomodoro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pomodoro
+//
+//  Created by Chase Choi on 2018/6/21.
+//  Copyright © 2018 Chase Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
