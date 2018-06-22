12ce0bbb332ea3f18712eedac6b0b27639a86737
ChaseChoi <caiguihao@gmail.com>
2018-06-22 17:18:57 +0800

    import Charts lib

---
+++ pomodoro/View Controllers/Records View Controller/RecordsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RecordsViewController.swift
+//  pomodoro
+//
+//  Created by Chase Choi on 2018/6/22.
+//  Copyright © 2018 Chase Choi. All rights reserved.
+//
+
+import UIKit
+
+class RecordsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
