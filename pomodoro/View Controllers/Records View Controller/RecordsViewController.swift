94c739e6826bc96b1aa56d0b8709d1e0fd52c7f2
ChaseChoi <caiguihao@gmail.com>
2018-06-23 20:11:56 +0800

    finish auto layout

--- pomodoro/View Controllers/Records View Controller/RecordsViewController.swift
+++ pomodoro/View Controllers/Records View Controller/RecordsViewController.swift
@@ -66,6 +66,8 @@ class RecordsViewController: UIViewController {
         
         pieChartView.chartDescription?.text = ""
         pieChartView.noDataText = "暂无数据"
+        pieChartView.noDataFont = UIFont.systemFont(ofSize: 17)
+        pieChartView.noDataTextColor = UIColor(red: 152/256, green: 166/256, blue: 195/256, alpha: 1)
         pieChartView.centerText = dateInfo
     }
     
